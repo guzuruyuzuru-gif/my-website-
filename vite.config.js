a58d8372fd9ca4266f7242684f6e9986987ce3b7
Yuzuru Ichihara <guzuruyuzuru@gmail.com>
2026-07-16 15:43:02 +0900

    頼む

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-      base: '/my-website/', 
+      base: '/my-website-/', 
   build: {
       outDir: 'docs',
     rollupOptions: {
